cb5102d7587dae559d83e62c7f3fbdc7be4a96d4
Luiz <luiz07.guga@gmail.com>
2026-05-03 13:01:33 -0300

    Arquivo index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+require 'dados.php';
+
+$categoriaSelecionada = $_GET['categoria'] ?? null;
+$escolhido = [];
+foreach($itens as $item){
+    if (!$categoriaSelecionada || $categoriaSelecionada == "Todas" || $item["modalidade"] == $categoriaSelecionada){
+        $escolhido[] = $item;
+    }
+}
+?>
+
+<a href="index.php?categoria=Todas">Todas</a>
+<a href="index.php?categoria=Musculação">Musculação</a>
+<a href="index.php?categoria=Crossfit">Crossfit</a>
+<a href="index.php?categoria=Natação">Natação</a>
+<a href="index.php?categoria=Artes Marciais">Artes Marciais</a>
+<a href="index.php?categoria=Dança">Dança</a>
+<a href="index.php?categoria=Funcional">Funcional</a>
+
+    <table border='1' cellpadding='10'>
+        <tr>
+            <th>Nome do produto</th>
+            <th>Modalidades</th>
+            <th>Valor da mensalidade</th>
+            <th>Vagas na turma</th>
+            <th>Número de meses</th>
+            <th>Ação</th>
+        </tr>
+
+        <?php foreach ($escolhido as $item):?>
+        <tr>
+            <th><?= $item["nome"]?></th>
+            <th><?= $item["modalidade"] ?></th>
+            <th>R$ <?= number_format($item["mensalidade"], 2, '.', ',')?></th>
+            <th><?= $item["vagas_turma"] ?></th>
+            <th><?= $item["numero_meses"]?></th>
+            <th><a href="detalhes.php?id=<?= $item['id'] ?>">Detalhes</a></th>
+        </tr>
+        <?php endforeach; ?>
+    </table>
